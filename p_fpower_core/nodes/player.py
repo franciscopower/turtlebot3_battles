3a79043377bd2f4460cd1d879538f91f9043f7f6
Francisco Power <franciscopower66@gmai.com>
2021-01-10 18:56:35 +0000

    read and display raw image

--- p_fpower_core/nodes/player.py
+++ p_fpower_core/nodes/player.py
@@ -7,23 +7,92 @@ import tf
 import tf2_ros
 import tf2_geometry_msgs
 from copy import  deepcopy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
+import cv2 as cv
 from math import pow, atan2, sqrt
+import numpy as np
 
-class Driver():
+class Player():
     def __init__(self, name):
         self.name = name
         
+        # get player color, hunter, prey ---------------
+        try:
+            names_red = rospy.get_param('/red_players')
+            names_green = rospy.get_param('/green_players')
+            names_blue = rospy.get_param('/blue_players')
+        except:
+            rospy.logfatal('No team parameters loaded')
+            exit(0)
+        else:
+            if self.name in names_red:
+                self.my_team = 'red'
+                self.my_team_players = names_red
+                self.prey_team_players = names_green
+                self.hunter_team_players = names_blue
+            elif self.name in names_green:
+                self.my_team = 'green'
+                self.my_team_players = names_green
+                self.prey_team_players = names_blue
+                self.hunter_team_players = names_red
+            elif self.name in names_blue:
+                self.my_team =  'blue'
+                self.my_team_players = names_blue
+                self.prey_team_players = names_red
+                self.hunter_team_players = names_green
+            else:
+                rospy.logfatal('Name is not in any team')
+                exit(0)
+        
+            print('My name is '+ self.name + '. I am team ' + self.my_team +
+                '. I am hunting ' + str(self.prey_team_players) + ' and being hunted by ' + str(self.hunter_team_players))
+        # --------------------------------------------
+        
+        # initialize cvbridge
+        self.cv_bridge = CvBridge()  
+        
+        # initialize publishers and subscribers         
         self.cmd_vel_pub = rospy.Publisher(self.name + '/cmd_vel', Twist, queue_size=10)    
-        self.goal_sub = rospy.Subscriber(self.name + "/move_base_simple/goal", PoseStamped, self.goal_callback)
-                
+        self.goal_sub = rospy.Subscriber(self.name + "/move_base_simple/goal", PoseStamped, self.goalCallback)
+        self.camera_sub = rospy.Subscriber(self.name + '/camera/rgb/image_raw', Image, self.imageCallback)
+              
+        # initialize transform buffer  
         self.tfBuffer = tf2_ros.Buffer(rospy.Duration(1200)) #? what is this time
         self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        
+        
     
-    def goal_callback(self, pose_stamped):
+    def goalCallback(self, pose_stamped):
         self.goal = pose_stamped
+        
+    def imageCallback(self, image):
+        self.image = self.cv_bridge.imgmsg_to_cv2(image, 'bgr8')
+        
+    def imageProcessor(self):
+        rate = rospy.Rate(10)
+        k = ''
+        while not rospy.is_shutdown() and k!=ord('q'):
+            
+            try:
+                cv.imshow('Camera raw', self.image)
+                
+                
+                
+                
+                
+                
+                k = cv.waitKey(1)
+            except:
+                pass
+            
+            rate.sleep
     
-    def drive(self):
+    def driveToGoal(self):
+        """
+        Creates twist message to drive robot to goal
+        """
         rate = rospy.Rate(10) # 10hz
         while not rospy.is_shutdown():
             twist = Twist()
@@ -48,36 +117,15 @@ class Driver():
             rate.sleep()
             
 if __name__ == '__main__':
-    rospy.init_node('p_fpower', anonymous=False)
+    rospy.init_node('green1', anonymous=False)
     name = rospy.get_name().strip('/')
-    
-    # names_red = rospy.get_param('/red_players')
-    # names_green = rospy.get_param('/green_players')
-    # names_blue = rospy.get_param('/blue_players')
-    
-    # if name in names_red:
-    #     my_team = 'red'
-    #     my_team_players = names_red
-    #     prey_team_players = names_green
-    #     hunter_team_players = names_blue
-    # elif name in names_green:
-    #     my_team = 'green'
-    #     my_team_players = names_green
-    #     prey_team_players = names_blue
-    #     hunter_team_players = names_red
-    # elif name in names_blue:
-    #     my_team =  'blue'
-    #     my_team_players = names_blue
-    #     prey_team_players = names_red
-    #     hunter_team_players = names_green
-    # else:
-    #     rospy.logfatal('Name is not in any team')
-        
-    # print('My name is '+ name + '. I am team ' + my_team +
-    #       '. I am hunting ' + str(prey_team_players) + ' and being hunted by ' + str(hunter_team_players))
                     
     try:
-        driver = Driver(name)
-        driver.drive()
+        player = Player(name)
+        player.imageProcessor()
+    except KeyboardInterrupt:
+        print("Shutting down vision node.")
+        cv.DestroyAllWindows()
     except rospy.ROSException:
         pass
+    
